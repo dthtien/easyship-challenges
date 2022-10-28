6c23dbe191e912b5ab4763acec369cac84596b2e
dthtien <tien.dau@employmenthero.com>
2022-10-28 15:29:10 +0700

    chore(shipment): refactor test

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,7 +2,8 @@ class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_items
 
-  def group_item_descriptions(order = :desc)
+  def group_item_descriptions(order)
+    order ||= :desc
     shipment_items.group(:description)
                   .select(:description, 'count(id) as count')
                   .order("count(id) #{order}")
