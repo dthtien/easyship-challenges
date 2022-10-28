73ea5e598ed13165f465c0699605ab04190b7ad5
dthtien <tien.dau@employmenthero.com>
2022-10-28 17:56:05 +0700

    chore(tracking): add tracking endpoint

---
+++ app/controllers/trackings_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class TrackingsController < ApplicationController
+  def show
+    response = aftership_adapter.tracking(shipment.tracking_number)
+
+    meta = response['meta']
+    if meta['code'] == 200
+      render json: response['data']
+    else
+      render json: meta, status: meta['code']
+    end
+  end
+
+  private
+
+  def aftership_adapter
+    @aftership_adapter ||= AftershipAdapter.new
+  end
+
+  def shipment
+    @shipment ||= Shipment.find_by(id: params[:shipment_id], company_id: params[:company_id])
+  end
+end
