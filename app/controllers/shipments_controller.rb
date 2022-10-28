c4e3122e573e0f02ae403d0e64b995b685ec57d1
dthtien <tien.dau@employmenthero.com>
2022-10-28 15:27:27 +0700

    chore(shipment): add show method

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -2,4 +2,9 @@ class ShipmentsController < ApplicationController
   def index
     @shipments = Shipment.all
   end
+
+  def show
+    @shipment = Shipment.find_by!(id: params[:id], company_id: params[:company_id])
+    @items = @shipment.group_item_descriptions(params[:items_order])
+  end
 end
