d62be5061d88e4da1e741878d90f25af2d989455
dthtien <tien.dau@employmenthero.com>
2022-10-28 17:09:24 +0700

    chore(tracking): add es tracking client

---
+++ spec/adapters/aftership_adapter_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+RSpec.describe AftershipAdapter, type: :adapter do
+  let(:adapter) { described_class.new }
+  describe '#tracking' do
+    let(:id) { '56a9f8144f6acb330d76a483' }
+    context 'when fetching success' do
+      before do
+        stub_request(:get, "https://api.aftership.com/v4/trackings/#{id}")
+          .to_return(body: success_response)
+      end
+
+      it do
+        response = adapter.tracking(id)
+
+        expect(response.dig('meta', 'code')).to be 200
+        expect(response['data']).not_to be_nil
+      end
+    end
+
+    context 'when fetching failed' do
+      before do
+        stub_request(:get, "https://api.aftership.com/v4/trackings/#{id}")
+          .to_return(body: failure_response)
+      end
+
+      it do
+        response = adapter.tracking(id)
+
+        expect(response.dig('meta', 'code')).to be 404
+        expect(response['data']).to be_nil
+      end
+    end
+  end
+
+  def success_response
+    File.open("#{Rails.root}/spec/fixtures/aftership/get_success_response.json", 'rb').read
+  end
+
+  def failure_response
+    File.open("#{Rails.root}/spec/fixtures/aftership/get_failure_response.json", 'rb').read
+  end
+end
+
