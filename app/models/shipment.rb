716af19fd651e19f7fb71690e759dce03ab8eca9
dthtien <tien.dau@employmenthero.com>
2022-10-28 13:35:10 +0700

    chore(shipment): add method group_item_descriptions

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,10 @@
 class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_items
+
+  def group_item_descriptions(order = :desc)
+    shipment_items.group(:description)
+                  .select(:description, 'count(id) as count')
+                  .order("count(id) #{order}")
+  end
 end
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -10,4 +10,59 @@ RSpec.describe Shipment, type: :model do
       expect(t.macro).to eq(:has_many)
     end
   end
+
+  describe '.group_item_descriptions' do
+    let(:shipment) { create(:shipment) }
+    let(:order) { :desc }
+    let(:result) { shipment.group_item_descriptions(order) }
+    before do
+      3.times do
+        create(:shipment_item, shipment: shipment, description: "iPad")
+      end
+      2.times do
+        create(:shipment_item, shipment: shipment, description: 'iPhone')
+      end
+      create(:shipment_item, shipment: shipment, description: 'Apple Watch')
+    end
+
+    context 'when order = desc' do
+      let(:order) { :desc }
+      specify do
+        expect(result.as_json(except: :id)).to eq [
+          {
+            'description' => 'iPad',
+            'count' => 3,
+          },
+          {
+            'description' => 'iPhone',
+            'count' => 2,
+          },
+          {
+            'description' => 'Apple Watch',
+            'count' => 1,
+          },
+        ]
+      end
+    end
+
+    context 'when order = asc' do
+      let(:order) { :asc }
+      specify do
+        expect(result.as_json(except: :id)).to eq [
+          {
+            'description' => 'Apple Watch',
+            'count' => 1,
+          },
+          {
+            'description' => 'iPhone',
+            'count' => 2,
+          },
+          {
+            'description' => 'iPad',
+            'count' => 3,
+          },
+        ]
+      end
+    end
+  end
 end
