de1d829532d8191a3ecae314282aab52b33cebbc
dthtien <tien.dau@employmenthero.com>
2022-10-29 23:03:07 +0700

    chore(shipment): add query_shipment_by_items_size method

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
 class Shipment < ApplicationRecord
+  RENDERING_FIELDS = %w[
+    id company_id destination_country origin_country tracking_number slug created_at
+  ].freeze
   belongs_to :company
   has_many :shipment_items
+
+  scope :query_shipment_by_items_size, lambda { |size|
+    left_joins(:shipment_items)
+      .select(*RENDERING_FIELDS)
+      .select('count(shipment_items.id) as items_count')
+      .group(*RENDERING_FIELDS).having('items_count = ?', size || 1)
+  }
 
   def group_item_descriptions(order = :desc)
     shipment_items.group(:description)
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -65,4 +65,43 @@ RSpec.describe Shipment, type: :model do
       end
     end
   end
+
+  describe '.query_shipment_by_items_size' do
+    let!(:shipment1) { create(:shipment) }
+    let!(:shipment2) { create(:shipment) }
+    let!(:shipment3) { create(:shipment) }
+    let!(:shipment) { create(:shipment) }
+    let(:order) { :desc }
+    let(:items_size) { 0 }
+    let(:result) { described_class.query_shipment_by_items_size(items_size) }
+    before do
+      3.times do
+        create(:shipment_item, shipment: shipment1, description: 'iPad')
+      end
+      2.times do
+        create(:shipment_item, shipment: shipment2, description: 'iPhone')
+      end
+      create(:shipment_item, shipment: shipment3, description: 'Apple Watch')
+    end
+
+    context 'when query items size = 0' do
+      let(:items_size) { 0 }
+      it { expect(result).to eq [shipment] }
+    end
+
+    context 'when query items size = 1' do
+      let(:items_size) { 1 }
+      it { expect(result).to eq [shipment3] }
+    end
+
+    context 'when query items size = 2' do
+      let(:items_size) { 2 }
+      it { expect(result).to eq [shipment2] }
+    end
+
+    context 'when query items size = 3' do
+      let(:items_size) { 3 }
+      it { expect(result).to eq [shipment1] }
+    end
+  end
 end
