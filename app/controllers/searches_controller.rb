acb91b7d4e4ba812b4ea0526ff0463c7c6168911
dthtien <tien.dau@employmenthero.com>
2022-10-30 12:52:00 +0700

    chore(task3): add todo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@
 
 class SearchesController < ApplicationController
   def create
+    # Todo: result N+1 query
     @shipments = Shipment.where(company_id: params[:company_id])
                          .query_shipment_by_items_size(params[:shipment_items_size].to_i)
 
