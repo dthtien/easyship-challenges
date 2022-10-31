d898b91268fae1a317c228bf3b8c9d9d017118ba
dthtien <tien.dau@employmenthero.com>
2022-10-31 14:28:00 +0700

    chore(shipment): handle more cases group item

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_items, dependent: :destroy
+  ORDER_DIRS = %w[desc asc].freeze
 
-  def group_item_descriptions(order = :desc)
+  def group_item_descriptions(order = nil)
+    order = ORDER_DIRS.include?(order.to_s.downcase) ? order : 'asc'
     shipment_items.group(:description)
                   .select(:description, 'count(id) as count')
                   .order("count(id) #{order}")
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -17,12 +17,8 @@ RSpec.describe Shipment, type: :model do
     let(:order) { :desc }
     let(:result) { shipment.group_item_descriptions(order) }
     before do
-      3.times do
-        create(:shipment_item, shipment: shipment, description: "iPad")
-      end
-      2.times do
-        create(:shipment_item, shipment: shipment, description: 'iPhone')
-      end
+      create_list(:shipment_item, 3, shipment: shipment, description: 'iPad')
+      create_list(:shipment_item, 2, shipment: shipment, description: 'iPhone')
       create(:shipment_item, shipment: shipment, description: 'Apple Watch')
     end
 
@@ -46,8 +42,7 @@ RSpec.describe Shipment, type: :model do
       end
     end
 
-    context 'when order = asc' do
-      let(:order) { :asc }
+    shared_examples 'orders ascending' do
       specify do
         expect(result.as_json(except: :id)).to eq [
           {
@@ -64,6 +59,23 @@ RSpec.describe Shipment, type: :model do
           },
         ]
       end
+    end
+
+    context 'when order = asc' do
+      let(:order) { :asc }
+
+      it_behaves_like 'orders ascending'
+    end
+
+    context 'when order = nil' do
+      let(:order) { nil }
+      it_behaves_like 'orders ascending'
+    end
+
+    context 'when order is something, not desc nor asc' do
+      let(:order) { 'ABC' }
+
+      it_behaves_like 'orders ascending'
     end
   end
 end
