4bcf1277d753f1c6bcf06eac95ff07d7b902f274
dthtien <tien.dau@employmenthero.com>
2022-11-02 10:29:25 +0700

    chore(shipment): preload shipment items on search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@
 class SearchesController < ApplicationController
   def create
     # TODO: resolve N+1 query
-    @shipments = Shipment.where(company_id: params[:company_id])
+    @shipments = Shipment.includes(:shipment_items).where(company_id: params[:company_id])
                          .query_shipment_by_items_size(params[:shipment_items_size].to_i)
 
     render :create
